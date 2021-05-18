823118ed8230d877a67651095013678467cf5211
WIN-QNIUGJC7Q0K\Administrator <faran.mazhar77@gmail.com>
2021-05-18 21:51:36 +0500

    Choose Next Way Point CPP behavior added

---
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -0,0 +1,13 @@
+// Developed by Scythe for testing purposes.
+
+
+#include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(WaypointIndex.SelectedKeyName);
+
+	return EBTNodeResult::Succeeded;
+}
---
+++ Source/TestingGrounds/ChooseNextWaypoint.h
@@ -0,0 +1,21 @@
+// Developed by Scythe for testing purposes.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "ChooseNextWaypoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+private:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+		struct FBlackboardKeySelector WaypointIndex;
+};
